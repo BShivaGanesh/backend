267af58f0b3762de4241de25a8f2a3ad6f2b0e93
BShivaGanesh <boggarapushivaganesh@gmail.com>
2024-11-21 20:32:50 +0530

    developed utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+//using promises
+//asyncHandler is a higher order function
+const asyncHandler = (requestHandler) => {
+    (req,res,next)=>{
+        Promise
+        .resolve(requestHandler(req,res,next))
+        .catch((err)=> next(err))
+    }
+ 
+}
+
+export  {asyncHandler}
+
+// we can do the same using try-catch also
+// const asyncHandler = (requestHandler) => async(req,res,next)=>{
+//     try{
+//         await requestHandler(req,res,next)
+//     }
+//     catch(error){
+//         res.status(error.code || 400).json({
+//             success : false,
+//             message : error.message
+//         })
+//     }
+// }
