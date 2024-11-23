9c56734fe0138ba4725eeb1933f5b7d20c17e0c9
BShivaGanesh <boggarapushivaganesh@gmail.com>
2024-11-23 09:56:00 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 //using promises
 //asyncHandler is a higher order function
 const asyncHandler = (requestHandler) => {
-    (req,res,next)=>{
+   return (req,res,next)=>{
         Promise
         .resolve(requestHandler(req,res,next))
         .catch((err)=> next(err))
