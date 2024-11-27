7565b0162ca89504f6899693a2de5f4001036503
BShivaGanesh <boggarapushivaganesh@gmail.com>
2024-11-27 15:21:13 +0530

    added new routes for user-controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -343,6 +343,14 @@ const updateUserCoverImage = asyncHandler(async(req, res) =>{
 
 })
 
+const getUserChannelProfile = asyncHandler(async (req , res) =>{
+   const {username} = req.params
+
+   if(!username?.trim()){
+    throw new ApiError(400 , "username is missing")
+   }
+})
+
 
 
 export {
